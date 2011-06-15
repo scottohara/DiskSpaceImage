374c18decc3d9da0db4536fd0eb5cf86413f7ac5
Scott O'Hara <scott@oharagroup.net>
2011-06-16 09:25:18 +1000

    Changed default config

--- DiskSpaceImage/setup.bat
+++ DiskSpaceImage/setup.bat
@@ -1,3 +1,3 @@
 c:
 cd \Windows\Microsoft.Net\Framework\v2.0.50727\
-installutil "C:\Documents and Settings\ScottO.NEXTGEN\My Documents\Visual Studio 2005\Projects\DiskSpaceImage\DiskSpaceImage\bin\Debug\DiskSpaceImage.exe"
+installutil "C:\Program Files\Disk Space Image\Debug\DiskSpaceImage.exe"
