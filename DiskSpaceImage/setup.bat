b5788f2b926fbb908bb0934aaf046265384774e7
Scott O'Hara <scott@oharagroup.net>
2016-03-19 21:23:54 +1100

    Update setup to .Net framework v4 and fix invalid config xml

--- DiskSpaceImage/setup.bat
+++ DiskSpaceImage/setup.bat
@@ -1,3 +1,3 @@
 c:
-cd \Windows\Microsoft.Net\Framework\v2.0.50727\
-installutil "C:\Program Files\Disk Space Image\Debug\DiskSpaceImage.exe"
+cd \Windows\Microsoft.Net\Framework\v4.0.30319\
+installutil "C:\Program Files\Disk Space Image\DiskSpaceImage.exe"
